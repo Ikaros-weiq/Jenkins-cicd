c8d0a7d1d1b89a93ad7f85b84fd55bfee341a868
Weiquan <weiq_cheah91@hotmail.com>
2023-03-05 21:15:44 +0800

    added controler

---
+++ src/main/java/com/Bookzy/website/WebSiteControler.java
@@ -0,0 +1,13 @@
+package com.Bookzy.website;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebSiteControler {
+
+    @RequestMapping
+    public String helloWorld(){
+        return "Hello World from Spring Boot";
+    }
+}
--- src/main/java/com/Bookzy/website/WebsiteApplication.java
+++ src/main/java/com/Bookzy/website/WebsiteApplication.java
@@ -1,13 +1,23 @@
 package com.Bookzy.website;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsiteApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
